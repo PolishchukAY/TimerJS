02bf3ea02b8ca0208069a817caa72c668cea0082
Polishchukay <polishchukay@gmail.com>
2020-04-15 12:24:15 +0300

    timer object

---
+++ js/scripts.js
@@ -0,0 +1,22 @@
+window.onload = function(){
+	
+	var div = document.querySelector('.items .item');
+	var timer = new Timer (60, div);
+	
+		
+	function Timer(time, elem){
+		
+		this.time = time;
+		this.elem = elem;
+		
+		this.tick = function(){
+			this.time--;
+			this.elem.innerHTML = this.time;
+		}
+	}
+	
+}
+
+
+
+
