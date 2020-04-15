a72fce924b350224f8aa7852f8e8fee903cc2983
Polishchukay <polishchukay@gmail.com>
2020-04-15 12:28:50 +0300

    timer on step tick

--- js/scripts.js
+++ js/scripts.js
@@ -3,6 +3,9 @@ window.onload = function(){
 	var div = document.querySelector('.items .item');
 	var timer = new Timer (60, div);
 	
+	timer.tick();	
+	timer.tick();			
+	timer.tick();	
 		
 	function Timer(time, elem){
 		
